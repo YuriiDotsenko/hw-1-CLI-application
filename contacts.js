80ea3f63cd079f2cae90834b34ec5203e0fb5f14
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-09-22 20:46:23 +0200

    зробив дз

--- contacts.js
+++ contacts.js
@@ -1,15 +1,8 @@
+const { v4 } = require("uuid");
 const fs = require("fs/promises");
 const path = require("path");
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
-
-const id = "";
-
-const newData = {
-  name: "",
-  email: "",
-  phone: "",
-};
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
@@ -26,57 +19,49 @@ const getContactById = async (id) => {
   return contacts[idx];
 };
 
-const updateContacts = async (contacts) => {
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-};
-
 const removeContact = async (id) => {
   const contacts = await listContacts();
-  const idx = contacts.findIndex((item) => item.id === id);
-  if (idx === -1) {
-    return null;
-  }
-  contacts.splice(idx, 1);
-  await updateContacts(contacts);
-  return true;
-};
+  const newContacts = contacts.filter((contact) => contact.id !== id);
 
-const addContact = async (data) => {
-  const contacts = await listContacts();
-  const newContacts = { ...data, id: 11 };
-  contacts.push(newContacts);
-  await updateContacts(contacts);
-  return newContacts;
-};
-
-(async () => {
   try {
-    const contacts = await listContacts();
-    console.log(contacts);
-
-    const oneContacts = await getContactById(id);
-    if (!oneContacts) {
-      throw new Error(`Контакт с id=${id} не найден`);
-    }
-    console.log(oneContacts);
-
-    const result = await removeContact(id);
-    if (!result) {
-      throw new Error(`Контакт с id=${id} не найден!`);
-    }
-    console.log("Удаление прошло успешно");
-
-    const newContact = await addContact(newData);
-    console.log(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    console.log("contact removed");
+    return newContacts;
   } catch (error) {
     console.log(error.message);
+    return null;
   }
-})();
+};
 
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   updateContacts,
-//   addContact,
-// };
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newId = v4();
+
+  contacts.push({
+    id: newId,
+    name,
+    email,
+    phone,
+  });
+
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.log("operation succeeded");
+    console.table({
+      id: newId,
+      name,
+      email,
+      phone,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-const listContacts = require("./contacts");
-const getContactById = require("./contacts");
-const addContact = require("./contacts");
-const removeContact = require("./contacts");
-const updateContacts = require("./contacts");
+const contacts = require("./contacts");
 
-const yargs = require("yargs");
 const argv = require("yargs").argv;
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      const contacts = listContacts();
-      console.table(contacts);
+      contacts.listContacts().then(console.table);
       break;
 
     case "get":
-      const contact = getContactById(id);
-      if (!contact) {
-        console.log(`Товар с id=${id} не найден`);
-      } else {
-        console.log(contact);
-      }
+      contacts.getContactById(id).then(console.table);
       break;
 
     case "add":
-      const add = addContact(name, email, phone);
-      console.log(add);
+      contacts.addContact(name, email, phone);
       break;
 
     case "remove":
-      const remove = removeContact(id);
-      console.log(remove);
+      contacts.removeContact(id);
       break;
 
     default:
